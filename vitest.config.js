962a9d99b0610f24e79653a81392cfecba626d13
Matt Travi <programmer@travi.org>
2026-04-11 10:31:55 -0500

    chore: initial commit

---
+++ vitest.config.js
@@ -0,0 +1,16 @@
+import {defineConfig} from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    restoreMocks: true,
+    mockReset: true,
+    resetEnv: true,
+
+    coverage: {
+      provider: 'v8',
+      reporter: ['lcov', 'text-summary', 'html'],
+      include: ['src/**'],
+      exclude: ['src/**/index.js']
+    }
+  }
+});
